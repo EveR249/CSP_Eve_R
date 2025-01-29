47d4e42f01fb9262d030ceb36acef62e6a6d9806
EveR249 <eve.richardson@ucas-edu.net>
2025-01-29 09:52:45 -0700

    Python Practice

---
+++ Variables/VariablesPractice.py
@@ -0,0 +1,41 @@
+#Eve Richardson, Variables Practice Python
+
+name = input("Eve")
+
+number = input("7")
+
+large_number = input("101")
+
+breakfast = input("Granola Bar")
+
+color = input("Sage Green")
+
+school = input("UCAS")
+
+year = input("2025")
+
+eye = input ("Greenish")
+
+age = ("14")
+
+subject = input("Biology")
+
+print(name)
+
+print(number)
+
+print(large_number)
+
+print(breakfast)
+
+print(color)
+
+print(school)
+
+print(year)
+
+print(eye)
+
+print(age) 
+
+print(subject)
