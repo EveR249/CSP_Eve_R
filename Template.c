22757fc52a3ccebb5e114fb050f706a78199202b
EveR249 <eve.richardson@ucas-edu.net>
2025-01-31 09:16:15 -0700

    Coded in C

--- Template.c
+++ Template.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
 char name[] = "Eve";
-int num = 14;
+int num = 7;
 float pi = 3.14;
+int num2 =  101;
+char breakfast[] = "Granola Bar";
+char color[] = "Sage green";
+char school[] = "UCAS";
+int year = 2025;
+char eye[] = "Greenish";
+int age = 14;
+char subject[] = "Biology";
 
 int main(void){
-    printf("Hello %s, my age is %d and my favorite number is %f\n", name, num, pi);
+    printf("%s\n", name);
     printf("%d\n", num);
-    printf("%f\n", pi);
+    printf("%d\n", num2);
+    printf("%s\n", breakfast);
+    printf("%s\n", color);
+    printf("%s\n", school);
+    printf("%d\n", year);
+    printf("%s\n", eye);
+    printf("%d\n", age);
+    printf("%s\n", subject);
     return 0;
 }
