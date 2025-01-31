29590023d664910d6b767fd98891bb91606894cb
EveR249 <eve.richardson@ucas-edu.net>
2025-01-31 09:35:10 -0700

    First programs

---
+++ FirstProgram.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+char name[] = ("What is your name: ");
+
+int main(void){
+    printf("%s, welcome to my program.\n", name);
+    return 0;
+}
---
+++ Variables/FirstProgram.py
@@ -0,0 +1,7 @@
+#Eve Richardson First Python Program
+
+name = input("Please tell me your name: ")
+
+print(name+ ", welcome to my program.")
+
+print("It doesn't do anything yet")
