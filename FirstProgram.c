fa9204fcce83535942eb07f45dc22e271346a481
EveR249 <eve.richardson@ucas-edu.net>
2025-01-31 09:53:18 -0700

    programming

--- FirstProgram.c
+++ FirstProgram.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-char name[] = ("What is your name: ");
+char name[20];
 
 int main(void){
+    printf("What is your name: \n");
+    fgets(name, sizeof(name), stdin);
     printf("%s, welcome to my program.\n", name);
+    printf("It doesn't do anything yet.\n");
     return 0;
 }
