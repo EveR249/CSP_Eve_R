efe3ea67f19dbe2fc562143b568880a43d0bb4f3
EveR249 <eve.richardson@ucas-edu.net>
2025-02-03 09:22:38 -0700

    fixed template for C

---
+++ Variables/Notes.c
@@ -0,0 +1,28 @@
+// Eve Richardson Variables Notes
+#include <stdio.h>
+
+char name[] = "Eve";
+int num = 7;
+float pi = 3.14;
+int num2 =  101;
+char breakfast[] = "Granola Bar";
+char color[] = "Sage green";
+char school[] = "UCAS";
+int year = 2025;
+char eye[] = "Greenish";
+int age = 14;
+char subject[] = "Biology";
+
+int main(void){
+    printf("%s\n", name);
+    printf("%d\n", num);
+    printf("%d\n", num2);
+    printf("%s\n", breakfast);
+    printf("%s\n", color);
+    printf("%s\n", school);
+    printf("%d\n", year);
+    printf("%s\n", eye);
+    printf("%d\n", age);
+    printf("%s\n", subject);
+    return 0;
+}
