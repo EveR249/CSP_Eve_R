e705e2d37cda2711928b619eb13059d88418a94f
EveR249 <eve.richardson@ucas-edu.net>
2025-02-04 09:53:16 -0700

    Exressions practice in C

---
+++ Expressions/ExpressionsPractice.c
@@ -0,0 +1,17 @@
+// Eve Richardson, Template
+#include <stdio.h>
+int eq1 = 7-24/8*4+6;
+int eq2 = 18/3-7+2*5;
+int eq3 = 6*4/12+72/8-9;
+int eq4 = (17-6/2)+4*3;
+int eq5 = -2*(1*4-2/2)+(6+2-3);
+int eq6 = -1*
+
+int main(void){
+    printf("%d\n", eq1);
+    printf("%d\n", eq2);
+    printf("%d\n", eq3);
+    printf("%d\n", eq4);
+    printf("%d\n", eq5);
+    return 0;
+}
