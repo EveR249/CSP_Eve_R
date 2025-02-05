611252971065ac8f3eca782b3a041e28fbe46f68
EveR249 <eve.richardson@ucas-edu.net>
2025-02-05 09:32:48 -0700

    Expressions in C

--- Expressions/ExpressionsPractice.c
+++ Expressions/ExpressionsPractice.c
@@ -1,11 +1,15 @@
 // Eve Richardson, Template
 #include <stdio.h>
+#include <math.h>
 int eq1 = 7-24/8*4+6;
 int eq2 = 18/3-7+2*5;
 int eq3 = 6*4/12+72/8-9;
 int eq4 = (17-6/2)+4*3;
 int eq5 = -2*(1*4-2/2)+(6+2-3);
-int eq6 = -1*
+int eq6 = -1*((3-4*7)/5)-2*24/6;
+int eq7 = (3*(int)pow(5,2)/15)-(5-(int)pow(2,2));
+int eq8 = ((int)pow(1,4)*(int)pow(2,2)+(int)pow(3,3))-(int)pow(2,5)/4;
+int eq9 = (int)pow((22/2-2*5),2)+(int)pow((4-6/6),2);
 
 int main(void){
     printf("%d\n", eq1);
@@ -13,5 +17,9 @@ int main(void){
     printf("%d\n", eq3);
     printf("%d\n", eq4);
     printf("%d\n", eq5);
+    printf("%d\n", eq6);
+    printf("%d\n", eq7);
+    printf("%d\n", eq8);
+    printf("%d\n", eq9);
     return 0;
 }
