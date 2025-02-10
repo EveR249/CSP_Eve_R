a8de5331e1299558099d0a00d3cdc9646d1dd0d4
EveR249 <eve.richardson@ucas-edu.net>
2025-02-10 16:13:50 -0700

    calc C

--- Expressions/FinancialCalc.c
+++ Expressions/FinancialCalc.c
@@ -19,6 +19,7 @@ int main(void){
     float savings = income/10;
     float spending = income - (rent+utilities+grocery+transpo+savings);
     float rentp = rent/income*100;
+    float spendingp = spending/income;
     float utilitiesp = utilities/income*100;
     float groceryp = grocery/income*100;
     float transpop = transpo/income*100;
@@ -27,5 +28,6 @@ int main(void){
     printf("You spend $%.2f on groceries and that is %.2f percent of your income.\n", grocery, groceryp);
     printf("You spend $%.2f on transportation and that is %.2f percent of your income.\n", transpo, transpop);
     printf("You should save $%.2f every month and that is 10 percent of your income.\n", savings);
+    printf("You have $%.2f left to spend and that is %.2f of your income.\n", spending, spendingp);
     return 0;
 }
