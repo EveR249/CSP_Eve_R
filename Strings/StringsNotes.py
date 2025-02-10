d38dd3b3b05c50b0af5858e0e1b376180ee0fe2b
EveR249 <eve.richardson@ucas-edu.net>
2025-02-10 09:53:06 -0700

    notes strings

---
+++ Strings/StringsNotes.py
@@ -0,0 +1,38 @@
+#Eve Richardson strings notes python
+
+#strings are words in programming (data type)"" ''
+
+#print("This is Eve's code")
+#print('This is Eves code')
+
+#name = input("What is your name? (First name only)\n").strip().lower()
+
+#name2 = input("What is your name? (First name only)\n").strip().upper()
+
+#name3 = input("What is your name? (First name only)\n").strip().capitalize()
+
+#say name is "     eVe   "
+
+#print(f"Hello {name} welcome")
+
+#print(f"Hello {name2} welcome")
+
+#print(f"Hello {name3} welcome")
+
+#syntax means the structure of how the language works
+#logic error is when you expect something but it gives you something else
+
+#num = input("Please give me a number:\n").strip()
+
+#print(f"{num}+1")
+#print(num + str(1))
+#print(num + "2")
+
+#num1 = 5*2+num
+
+#print(num1)
+
+sent = "The quick brown fox jumps over the lazy dog"
+
+print(sent.find("fox"))
+print(sent.split)
