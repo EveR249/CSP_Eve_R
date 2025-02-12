6214d7f2f96a34b133b80b3117d009116d86943d
EveR249 <eve.richardson@ucas-edu.net>
2025-02-12 09:52:38 -0700

    silly sentence python

---
+++ Strings/SillySentence.py
@@ -0,0 +1,7 @@
+#Eve Richardson Silly sentence Python
+
+adj = input("Write an adjective: ")
+place = input("Write the name of a place: ")
+verb = input("Write a verb: ")
+
+print(f"Bob was eating the {adj} pie when suddenly a clown come running at him from {place} and it started {verb}!")
