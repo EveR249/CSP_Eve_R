08bedf15cfb9c3fc92dc486faa44f40d149f6035
EveR249 <eve.richardson@ucas-edu.net>
2025-02-12 09:19:41 -0700

    finished practice decor

---
+++ Strings/NameDecorator.py
@@ -0,0 +1,7 @@
+#Eve Richardson Name Decorator Python
+
+name = input("What is your name? (First name only please)\n").strip().upper()
+smile1 = ":) :) "
+smile2 = " :) :)"
+
+print(smile1+name+smile2)
