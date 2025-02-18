7ad47700f835c384279447556505e36f5e52b9a5
EveR249 <eve.richardson@ucas-edu.net>
2025-02-18 09:35:56 -0700

    functionsnotespy

---
+++ Functions/Notes.py
@@ -0,0 +1,18 @@
+#Eve Richardson functions notes python
+
+#def add():
+#    numOne = int(input("Give me a number:\n"))
+#    numTwo = int(input("Give me a number:\n"))
+#    print(numOne+numTwo)
+
+#number = int(input("Give me a number:\n"))
+
+#add()
+
+def user(word):
+   return input(f"Tell me a {word}:\n")
+
+name = user("name")
+verb = user("verb")
+place = user("place")
+print(f"{name} was {verb} and somehow got to {place}")
