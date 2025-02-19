28faf298929d6dcf915a0b360fe18cbfd295a5bb
EveR249 <eve.richardson@ucas-edu.net>
2025-02-19 09:21:16 -0700

    notes in C

--- Functions/Notes.c
+++ Functions/Notes.c
@@ -1,13 +1,13 @@
 // Eve Richardson, functions notes C
 #include <stdio.h>
 
-char input(char type[20]){
-    char answer[50];
-    printf("Please give me a %s:\n",
-    type);
-    scanf("%s", answer);
-    return answer;
-}
+//void word(char type[50]){
+    //char answer[50];
+    //printf("Please give me a %s:\n",
+    //type);
+    //getStr(answer, sizeof(answer)-1);
+    //return answer;
+//}
 
 void add(){
     printf("%d\n",6+8);
@@ -19,14 +19,14 @@ void add2(int numOne, int numTwo){
 }
 
 int main(void){
-    printf("Hello World\n");
-    add2(3,2);
-    add2(4,9);
-    add2(9,0);
-    add();
+    //printf("Hello World\n");
+    //add2(3,2);
+    //add2(4,9);
+    //add2(9,0);
+    //add();
     input("name");
     input("verb");
     input("place");
-    printf("%s was %s until they somehow reached %s\n", input("name"), input("verb"), input("place"));
+    printf("%s was %s until they somehow reached %s\n", word("name", 50), word("verb", 50), inpwordut("place", 50));
     return 0;
 }
