5103967c2cc5533df63923c2480ac3c413d2a5ad
EveR249 <eve.richardson@ucas-edu.net>
2025-02-20 09:48:35 -0700

    hello world py

---
+++ Functions/HelloWorldUpdate.py
@@ -0,0 +1,14 @@
+#Eve Richardson Hello world Update Python
+
+
+
+def names():
+    name = input("What is your name?\n")
+    
+    print(f"Hello {name}")
+
+names()
+names()
+names()
+names()
+names()
