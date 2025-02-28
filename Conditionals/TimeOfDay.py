5b794622d8a1ce0bcb4a4ffb6ac46826ab8caee6
EveR249 <eve.richardson@ucas-edu.net>
2025-02-28 09:13:38 -0700

    time of day python

--- Conditionals/TimeOfDay.py
+++ Conditionals/TimeOfDay.py
@@ -3,15 +3,13 @@
 import time
 
 current = time.time()
-
-
-
 now = time.ctime(current)
-
-
-
 local_time = time.localtime(current)
-
-minutes = local_time.tm_min
-
 hours = local_time.tm_hour
+
+if hours <= 11:
+    print("Good morning!\n")
+elif hours <= 15:
+    print("Good afternoon!\n")
+else:
+    print("Good evening!\n")
