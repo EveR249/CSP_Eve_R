0ab21eb24404ef3cb1b2ee740c6679fda8cb4f07
EveR249 <eve.richardson@ucas-edu.net>
2025-03-07 09:17:40 -0700

    FizzBuzz in C

---
+++ Loops/FizzBuzz.c
@@ -0,0 +1,19 @@
+// Eve Richardson, FizzBuzz in C
+#include <stdio.h>
+
+int num;
+
+int main(void){
+     for(num=0; num<51; num++){
+        if(num%3==0 && num%5==0){
+             printf("FizzBuzz\n");
+        }else if (num%3==0){
+            printf("Fizz\n");
+        }else if (num%5==0){
+            printf("Buzz\n");
+        }else{
+            printf("%d\n", num);
+        }
+    }
+    return 0;
+}
