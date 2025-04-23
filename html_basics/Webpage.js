e65e87e26fabce5c526932e68715cb47cae70d2f
EveR249 <eve.richardson@ucas-edu.net>
2025-04-23 16:34:24 -0600

    webpage

--- html_basics/Webpage.js
+++ html_basics/Webpage.js
@@ -15,14 +15,15 @@ function show(){
 }
 
 let images = ["https://m.media-amazon.com/images/I/91u+X+8g5hL.jpg","https://karabullockart.com/wp-content/uploads/2020/01/Half-In-Half-Out-10x10-1.jpeg"]
+let counter = 0
 
-count = 0
 function paint(){
-    document.getElementById("image").src = images
     [countif]
-    if(count === 2){
-        count = 0
+    if(counter < images.length){
+        document.getElementById("image").src = images[counter]
+        counter +=1
     }else{
-        count +=1
+        counter = 0
+        document.getElementById("image").src = images[counter]
     }
 }
