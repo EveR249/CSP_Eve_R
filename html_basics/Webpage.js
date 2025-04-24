c79321df50b26d44fe10b266553b66245a383302
EveR249 <eve.richardson@ucas-edu.net>
2025-04-24 09:53:14 -0600

    web

--- html_basics/Webpage.js
+++ html_basics/Webpage.js
@@ -1,29 +1,41 @@
 document = FirstWebPage.html
 
-function flash(){
-    document.getElementById("button").style.backgroundColor = "teal"
-    document.getElementById("button").style.color = "white"
+let images = ["https://m.media-amazon.com/images/I/91u+X+8g5hL.jpg","https://karabullockart.com/wp-content/uploads/2020/01/Half-In-Half-Out-10x10-1.jpeg"]
+count = 0
+function paint(){
+    document.getElementById("three").src = images
+    [countif]
+    if(count === 2){
+        count = 0
+    }else{
+        count +=1
+    }
 }
 
-function flashtwo(){
-    document.getElementById("button").style.backgroundColor = "white"
-    document.getElementById("button").style.color = "teal"
+
+function flash(){
+    document.getElementById("shw").style.backgroundColor = "white"
+    document.getElementById("shw").style.color = "teal"
+}
+
+
+function normal(){
+    document.getElementById("shw").style.color = "white"
+    document.getElementById("shw").style.backgroundColor = "teal"
+}
+
+function press(){
+    document.getElementById("shw").style.color = "purple"
+    document.getElementById("shw").style.backgroundColor = "lavender"
 }
 
 function show(){
-    document.getElementById("extra").style.display = "block"
-}
-
-let images = ["https://m.media-amazon.com/images/I/91u+X+8g5hL.jpg","https://karabullockart.com/wp-content/uploads/2020/01/Half-In-Half-Out-10x10-1.jpeg"]
-let counter = 0
-
-function paint(){
-    [countif]
-    if(counter < images.length){
-        document.getElementById("image").src = images[counter]
-        counter +=1
+    if(document.getElementById("ex").style.display != "block"){
+        document.getElementById("ex").style.display = "block"
+        document.getElementById("shw").innerHTML = "Show Less"
     }else{
-        counter = 0
-        document.getElementById("image").src = images[counter]
+        document.getElementById("ex").style.display = "none"
+        document.getElementById("shw").innerHTML = "Show More"
     }
+    
 }
